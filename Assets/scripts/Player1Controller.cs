daba73c67e3826564ea6faf0e5f441510b81c4ee
Master_Cris <crisliberty12@gmail.com>
2019-07-17 23:03:12 -0400

    cree los detalles principales del nivel monocromatico.

---
+++ Assets/scripts/Player1Controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Controller : MonoBehaviour
+{
+    public float speed{get; set;}
+    public float jump{ get; set; }
+
+    public float hola = 10; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.speed = 15;
+        this.jump = 10;
+        print("Starting");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        this.transform.position += new Vector3(hInput * speed * Time.deltaTime, 0, 0);
+
+        float vInput = Input.GetAxis("Vertical");
+        this.transform.position += new Vector3(0, vInput * jump * Time.deltaTime, 0);
+    }
+}
